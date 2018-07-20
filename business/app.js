8f0ace041ee02af9dc170f90e92cf78c1a0d3ee4
Marco Ricupero <marco.ricupero@gmail.com>
2018-07-20 08:54:34 +0200

    [business] Added root /api/ for services

--- business/app.js
+++ business/app.js
@@ -23,7 +23,7 @@ app.use(compression({
     level : 9
   }));
 
-app.use('/', rootRouter);
+app.use('/api/', rootRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
