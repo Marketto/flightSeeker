85ec1853a2346a57469cf3e21c3740a389467404
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-02 14:00:57 +0200

    upd cfg

--- install.sh
+++ install.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-sh ./database/restore.sh
-#npm install --production ./business
-#npm install --production ./presentation
-npm build ./presentation
+sh ./database/restore.sh && npm build ./presentation
