a154f62155c7bf6a7e1d7b61b5664ddc9b5a0613
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-02 12:05:53 +0200

    install & packages upd

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sh ./database/restore.sh
-npm install --production ./business
-npm install --production ./presentation
+#npm install --production ./business
+#npm install --production ./presentation
 npm build ./presentation
