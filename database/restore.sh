17bf70f0f72ea2806784aaafbafcea7e19ead7c9
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-05 13:12:13 +0200

    [database] fix: missing flights backup step in restore

--- database/restore.sh
+++ database/restore.sh
@@ -2,6 +2,7 @@
 mkdir dump/flightSeeker
 mongodump --db flightSeeker --collection users &>/dev/null
 mongodump --db flightSeeker --collection flightLists &>/dev/null
+mongodump --db flightSeeker --collection flights &>/dev/null
 cp -rf ./flightSeeker/*.* ./dump/flightSeeker
 mongo flightSeeker --eval "db.dropDatabase()" &>/dev/null
 mongorestore --dir ./dump
