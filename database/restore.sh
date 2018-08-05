51510d0692447e937955cbc1818830b07569eb3f
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-05 12:55:22 +0200

    [database] ignored output for steps related to existing db

--- database/restore.sh
+++ database/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir dump
-mongodump --db flightSeeker --collection users
-mongodump --db flightSeeker --collection flightLists
+mkdir dump >/dev/null
+mongodump --db flightSeeker --collection users >/dev/null
+mongodump --db flightSeeker --collection flightLists >/dev/null
 cp -rf ./flightSeeker/*.* ./dump/flightSeeker
 mongo flightSeeker --eval "db.dropDatabase()"
 mongorestore --dir ./dump
