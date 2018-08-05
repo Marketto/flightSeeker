6aa33b35ccc1d9d1cc0ff6221a3b32d625100bfd
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-05 12:59:22 +0200

    [database] fixed issues related to fresh new db

--- database/restore.sh
+++ database/restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir dump >/dev/null
-mongodump --db flightSeeker --collection users >/dev/null
-mongodump --db flightSeeker --collection flightLists >/dev/null
+mkdir dump/flightSeeker
+mongodump --db flightSeeker --collection users &>/dev/null
+mongodump --db flightSeeker --collection flightLists &>/dev/null
 cp -rf ./flightSeeker/*.* ./dump/flightSeeker
-mongo flightSeeker --eval "db.dropDatabase()"
+mongo flightSeeker --eval "db.dropDatabase()" &>/dev/null
 mongorestore --dir ./dump
