26d7751dc44422ea9d3af0f043b6565cbecf9de6
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-08 10:15:27 +0200

    [database] fixed restore script

--- database/restore.sh
+++ database/restore.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-mkdir ./dump/flightSeeker &>/dev/null
+mkdir dump &>/dev/null
+mkdir dump/flightSeeker &>/dev/null
 mongodump --db flightSeeker --collection users &>/dev/null
 mongodump --db flightSeeker --collection flightLists &>/dev/null
 mongodump --db flightSeeker --collection flights &>/dev/null
 cp -rf ./flightSeeker/*.* ./dump/flightSeeker
-mongo flightSeeker --eval "db.dropDatabase()" &>/dev/null
+mongo flightSeeker --eval "db.dropDatabase()" --quiet &>/dev/null
 mongorestore --dir ./dump
 rm -rf ./dump
