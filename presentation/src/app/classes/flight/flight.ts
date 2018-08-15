3ae714650c8f8f943b1aedea58ccf99cf7954355
Marco Ricupero <marco.ricupero@gmail.com>
2018-08-15 18:20:05 +0200

    [presentation] added meal icon (Free no link back) [presentation] added styles for meals [presentation] add type Meal, auto parse of meal types

--- presentation/src/app/classes/flight/flight.ts
+++ presentation/src/app/classes/flight/flight.ts
@@ -3,13 +3,45 @@ import { FlightVector } from './flight-vector';
 import * as moment from 'moment-timezone';
 import { Duration } from 'moment';
 
+type Meal =
+   'B'  // Breakfast
+  |'K'  // Continental breakfast
+  |'L'  // Lunch
+  |'D'  // Dinner
+  |'S'  // Snack
+  |'M'  // Generic Meal
+  |'R'  // Refreshment
+  |'C'  // Alcoholic beverages complimentary
+  |'F'  // Food for purchase
+  |'P'  // Alcoholic beverages for purchase
+  |'Y'  // Duty free sales available
+  |'N'  // No meal service
+  |'V'  // Refreshments for purchase
+  |'G'  // Food and beverages for purchase
+
+  |'OO'  // Cold meal
+  |'OM'  // Cold meal
+  |'OB'  // Cold Breakfast
+  |'OK'  // Cold Continental breakfast
+  |'OL'  // Cold Lunch
+  |'OD'  // Cold Dinner
+
+  |'HH'  // Hot meal
+  |'HM'  // Hot meal
+  |'HB'  // Hot Breakfast
+  |'HK'  // Hot Continental breakfast
+  |'HL'  // Hot Lunch
+  |'HD'  // Hot Dinner
+
+  ;
+
 export class Flight {
   public departure: FlightVector;
   public arrival: FlightVector;
   public airline: Airline;
   public duration: Duration;
   public 'number': number;
-  public meals: string;
+  public meals: Meal[];
   public uuid: string;
 
   constructor(obj?: any) {
@@ -21,7 +53,7 @@ export class Flight {
         moment.duration(this.arrival.dateTime.diff(this.departure.dateTime)) :
         undefined;
       this.number = obj.number;
-      this.meals = obj.meals;
+      this.meals = obj.meals ? obj.meals.match(/([OH]?[BCDFGKLMNPRSVY]|HH|OO)/ig) : [];
       this.uuid = obj.uuid;
     }
   }
