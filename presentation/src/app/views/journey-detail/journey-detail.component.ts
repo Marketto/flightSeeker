56b74027cd7f0693d72781920187b0eb88e22dac
Marco Ricupero <marco.ricupero@gmail.com>
2018-09-06 23:06:35 +0200

    [presentation] fixed rest time for flight onger than 6h

--- presentation/src/app/views/journey-detail/journey-detail.component.ts
+++ presentation/src/app/views/journey-detail/journey-detail.component.ts
@@ -42,7 +42,7 @@ export class JourneyDetailComponent implements OnInit {
 
   private searchReturnFlight(flight: Flight): void {
     const returnDepartureDateTime: Moment = (flight.duration.asHours() > 6 ?
-      moment(flight.arrival.dateTime).add(22, 'h')
+      moment(flight.arrival.dateTime).add(24, 'h')
       : moment(flight.arrival.dateTime).add(40, 'm')).tz(flight.arrival.airport.timeZone);
 
     this.flightService.search(
