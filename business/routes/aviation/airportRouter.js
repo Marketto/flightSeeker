a0e746d281ad27e264d447620a1e8c7dd3a82311
Marco Ricupero <marco.ricupero@gmail.com>
2019-08-31 21:47:54 +0200

    City name spell check

--- business/routes/aviation/airportRouter.js
+++ business/routes/aviation/airportRouter.js
@@ -14,11 +14,6 @@ function reqAirport(req, res, next) {
     const notInCity = (req.query.notInCity || "").trim() || null;
     const iataAirport = (req.params.iataAirport || "").trim() || null;
 
-    
-    const startsWithRegExp = startsWith ? {
-        '$regex': new RegExp(`^${escapeStringRegexp(startsWith)}`, 'i')
-      } : null;
-
     const reachableAirports = res.routesData && [[]].concat(res.routesData.map(r=>r.iataAirports))
       .reduce((a,b)=>a.concat(b))
       .filter((iata, idx, airports) => iata !== req.params.iataDeparture && airports.indexOf(iata)===idx).sort();
@@ -29,16 +24,9 @@ function reqAirport(req, res, next) {
       {
         '$and' : [
           startsWith && {
-            '$or' : [
-              {
-                '$text' : {
-                  '$search': startsWith
-                }
-              },
-              {
-                'iata': startsWithRegExp
-              }
-            ]
+            '$text' : {
+              '$search': startsWith
+            }
           }, notInCity  && {
             'cityIata': {
               '$ne': notInCity
@@ -67,6 +55,63 @@ function reqAirport(req, res, next) {
   });
 }
 
+function reqAirportSpellChecker(req, res, next) {
+  console.log('[reqAirportSpellChecker]');
+  const startsWith = (req.query.startsWith || "").trim() || null;
+  if (res.airportData.length > 0 || !startsWith) {
+    next();
+  }
+
+  const spellCheckerRegexpGenerator = (text) => {
+    const fillMatcher = "\\w";
+    const matchers = escapedStartsWith.split('')
+      .map((c, i, a) => c.padStart(i * fillMatcher.length + c.length , fillMatcher) + "".padEnd((a.length - i - 1) * fillMatcher.length, fillMatcher)).join('|')
+      .join('|');
+    return new RegExp(`^(?:${matchers})$`, 'ui');
+  };
+
+  mongo().then(db => {
+    const notInCity = (req.query.notInCity || "").trim() || null;
+    const escapedStartsWith = escapeStringRegexp(startsWith);
+    const matchingRegExp = spellCheckerRegexpGenerator(escapedStartsWith);
+
+    const reachableAirports = res.routesData && [[]].concat(res.routesData.map(r=>r.iataAirports))
+      .reduce((a,b)=>a.concat(b))
+      .filter((iata, idx, airports) => iata !== req.params.iataDeparture && airports.indexOf(iata)===idx).sort();
+
+    const query = {
+        '$and' : [
+          {
+            cityNames: {
+              $regex: matchingRegExp
+            }
+          }, notInCity  && {
+            'cityIata': {
+              '$ne': notInCity
+            }
+          }, reachableAirports && {
+            'iata' : {
+              '$in': reachableAirports
+            }
+          }
+        ].filter(c=>!!c)
+      };
+    
+    const airportsCursor = db.collection("airports").find(query);
+    airportsCursor.project({_id: 0, cityNames: 0});
+    airportsCursor.toArray().then(result => {
+      res.airportData = result || [];
+      next();
+    }, err => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+  }, err => {
+    console.error(err);
+    res.sendStatus(500);
+  });
+}
+
 function resAirport(req, res) {
   console.log('[resAirline]');
   if (res.airportData.length > 0) {
@@ -77,6 +122,6 @@ function resAirport(req, res) {
 }
 
 router.get('/', reqAirport, resAirport);
-router.get(`/:iataAirport(${AIRPORT_ROUTE_MATCHER})`, reqAirport, resAirport);
+router.get(`/:iataAirport(${AIRPORT_ROUTE_MATCHER})`, reqAirport, reqAirportSpellChecker, resAirport);
 
 module.exports = router;
